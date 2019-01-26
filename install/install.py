1445e0a9d9e71262a98d1b1ddb69f47e0a4be557
Eldor Bekpulatov <31895957+eldorbekpulatov@users.noreply.github.com>
2019-01-26 16:52:33 -0500

    Add files via upload

---
+++ install/install.py
@@ -0,0 +1,16 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-jupyter", help="Installs Jupyter Notebook dependencies.", action="store_true")
+args = parser.parse_args()
+
+if(args.jupyter):
+    cmd = "conda env create -f environment_with_jupyter.yml"
+else:
+    md = "conda env create -f environment_without_jupyter.yml"
+
+try:    
+    os.system(cmd)
+except:
+    print("Could not install the specidied environment!")
